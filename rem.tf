5597659b12f4af82770b3f29d7579885f1fa06de
kunjjain11 <42216225+kunjjain11@users.noreply.github.com>
2023-06-02 12:17:34 +0530

    Create rem.tf

---
+++ rem.tf
@@ -0,0 +1,30 @@
+resource "aws_s3_bucket" "kj" {
+  bucket = "my-terraform-bucket-trng5"
+
+  tags = {
+    Name        = "Day5 bucket"
+    Environment = "KJ"
+  }
+}
+
+resource "aws_security_group" "elb-sg" {
+    name = "elb-sg"
+
+    ingress {
+        from_port = 443
+        to_port = 443
+        protocol = "tcp"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+        from_port = 0
+        to_port = 0
+        protocol = "-1"
+        cidr_blocks = ["0.0.0.0/0"]
+    }
+}
+
+resource "aws_eip" "lb-eip" {
+  vpc = true
+}
